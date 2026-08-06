050528fb89ae9b9fec4e997f629da0fdff3d1484
rizzdev101 <abdulmajidalkarim343@gmail.com>
2026-08-06 08:05:16 +0700

    .

--- subdomain/src/app/(public)/articles/page.tsx
+++
@@ -1,9 +0,0 @@
-// P2: Server component - fetch articles from Sanity (with fallback hardcoded)
-import { getAllArticles } from "@/sanity/fetchers";
-import { ArticlesPageClient } from "./ArticlesPageClient";
-
-export default async function ArticlesPage() {
-  const articles = await getAllArticles();
-
-  return <ArticlesPageClient articles={articles} />;
-}
