300570d0e506b5df9f2435023f895ceeecf5f454
rizzdev101 <abdulmajidalkarim343@gmail.com>
2026-08-06 08:06:16 +0700

    .

--- subdomain/src/app/(public)/portfolio/page.tsx
+++
@@ -1,9 +0,0 @@
-// P2: Server component - fetch projects from Sanity (with fallback hardcoded)
-import { getAllProjects } from "@/sanity/fetchers";
-import { PortfolioPageClient } from "./PortfolioPageClient";
-
-export default async function PortfolioPage() {
-  const projects = await getAllProjects();
-
-  return <PortfolioPageClient projects={projects} />;
-}
